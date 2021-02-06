4e89de04050c1efe234c057921bd9f8a2876f51d
NguyenThanhCong <thanhcong1431995@gmail.com>
2021-02-06 15:28:28 +0700

    temp commit

--- demo/debug_demo/debug_demo.ino
+++ demo/debug_demo/debug_demo.ino
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "debug.h"
 
+volatile int led_cycle=0;
+
 void console_handler(char *result){
   if(__check_cmd("hello")){
     debug("hi there\n");
@@ -22,6 +24,18 @@ void console_handler(char *result){
       debug("You just enter %d and %d\n", x, y);
     }
   }
+  else if(__check_cmd("led frequency ")){
+    int freq;
+    if(1==sscanf(__param_pos("led frequency "), "%d", &freq)){
+      debug("set led frequency to %d Hz\n", freq);
+      if(0==freq){
+        led_cycle=0;
+      }
+      else{
+        led_cycle=1000/freq/2;
+      }
+    }
+  }
   else{
     debug("What's up?\n");
   }
@@ -29,9 +43,21 @@ void console_handler(char *result){
 
 void setup() {
     debug_init(&Serial, 9600, console_handler);
+    pinMode(LED_BUILTIN, OUTPUT);
     debug("Hello world\n");
+}
+void led_process(){
+  static uint32_t tick=0;
+  if(led_cycle>0){
+    if(millis()-tick>led_cycle){
+      tick=millis();
+      //toggle led
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    }
+  }
 }
 
 void loop() {
+  led_process();
   debug_polling();
 }
